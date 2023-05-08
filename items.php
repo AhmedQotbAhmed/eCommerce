12f230154898848557347f5653fa6715d43e771a
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2023-05-08 17:48:13 +0300

    Add item

--- items.php
+++ items.php
@@ -17,13 +17,227 @@
 		$do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
 		if ($do == 'Manage') {
-            echo'wellcome'
+			
+            echo'wellcome';
 
 
-		} elseif ($do == 'Add') {
+		} elseif ($do == 'Add') { ?>
+
+<h1 class="text-center">Add New Item</h1>
+<div class="container">
+    <form class="form-horizontal" action="?do=Insert" method="POST">
+        <!-- Start Name Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Name</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="name" class="form-control" required="required"
+                    placeholder="Name of The Item" />
+            </div>
+        </div>
+        <!-- End Name Field -->
+        <!-- Start Description Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Description</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="description" class="form-control" required="required"
+                    placeholder="Description of The Item" />
+            </div>
+        </div>
+        <!-- End Description Field -->
+        <!-- Start Price Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Price</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="price" class="form-control" required="required"
+                    placeholder="Price of The Item" />
+            </div>
+        </div>
+        <!-- End Price Field -->
+        <!-- Start Country Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Country</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="country" class="form-control" required="required"
+                    placeholder="Country of Made" />
+            </div>
+        </div>
+        <!-- End Country Field -->
+        <!-- Start Status Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Status</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="status">
+                    <option value="0">...</option>
+                    <option value="1">New</option>
+                    <option value="2">Like New</option>
+                    <option value="3">Used</option>
+                    <option value="4">Very Old</option>
+                </select>
+            </div>
+        </div>
+        <!-- End Status Field -->
+        <!-- Start Members Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Member</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="member">
+                    <option value="0">...</option>
+                    <?php
+									$allMembers = getAllFrom("*", "users", "", "", "UserID");
+									foreach ($allMembers as $user) {
+										echo "<option value='" . $user['UserID'] . "'>" . $user['Username'] . "</option>";
+									}
+								?>
+                </select>
+            </div>
+        </div>
+        <!-- End Members Field -->
+        <!-- Start Categories Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Category</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="category">
+                    <option value="0">...</option>
+                    <?php
+									$allCats = getAllFrom("*", "categories", "where parent = 0", "", "ID");
+									foreach ($allCats as $cat) {
+										echo "<option value='" . $cat['ID'] . "'>" . $cat['Name'] . "</option>";
+										$childCats = getAllFrom("*", "categories", "where parent = {$cat['ID']}", "", "ID");
+										foreach ($childCats as $child) {
+											echo "<option value='" . $child['ID'] . "'>--- " . $child['Name'] . "</option>";
+										}
+									}
+								?>
+                </select>
+            </div>
+        </div>
+        <!-- End Categories Field -->
+        <!-- Start Tags Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Tags</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="tags" class="form-control" placeholder="Separate Tags With Comma (,)" />
+            </div>
+        </div>
+        <!-- End Tags Field -->
+        <!-- Start Submit Field -->
+        <div class="form-group form-group-lg">
+            <div class="col-sm-offset-2 col-sm-10">
+                <input type="submit" value="Add Item" class="btn btn-primary btn-sm" />
+            </div>
+        </div>
+        <!-- End Submit Field -->
+    </form>
+</div>
+
+<?php
+
+		} elseif ($do == 'Insert') {
+
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+				echo "<h1 class='text-center'>Insert Item</h1>";
+				echo "<div class='container'>";
+
+				// Get Variables From The Form
+
+				$name		= $_POST['name'];
+				$desc 		= $_POST['description'];
+				$price 		= $_POST['price'];
+				$country 	= $_POST['country'];
+				$status 	= $_POST['status'];
+				$member 	= $_POST['member'];
+				$cat 		= $_POST['category'];
+				$tags 		= $_POST['tags'];
+
+				// Validate The Form
+
+				$formErrors = array();
+
+				if (empty($name)) {
+					$formErrors[] = 'Name Can\'t be <strong>Empty</strong>';
+				}
+
+				if (empty($desc)) {
+					$formErrors[] = 'Description Can\'t be <strong>Empty</strong>';
+				}
+
+				if (empty($price)) {
+					$formErrors[] = 'Price Can\'t be <strong>Empty</strong>';
+				}
+
+				if (empty($country)) {
+					$formErrors[] = 'Country Can\'t be <strong>Empty</strong>';
+				}
+
+				if ($status == 0) {
+					$formErrors[] = 'You Must Choose the <strong>Status</strong>';
+				}
+
+				if ($member == 0) {
+					$formErrors[] = 'You Must Choose the <strong>Member</strong>';
+				}
+
+				if ($cat == 0) {
+					$formErrors[] = 'You Must Choose the <strong>Category</strong>';
+				}
+
+				// Loop Into Errors Array And Echo It
+
+				foreach($formErrors as $error) {
+					echo '<div class="alert alert-danger">' . $error . '</div>';
+				}
+
+				// Check If There's No Error Proceed The Update Operation
+
+				if (empty($formErrors)) {
+
+					// Insert Iteminfo In Database
+
+					$stmt = $con->prepare("INSERT INTO 
+
+						items(Name, Description, Price, Country_Made, Status, Add_Date, Cat_ID, Member_ID, tags)
+
+						VALUES(:zname, :zdesc, :zprice, :zcountry, :zstatus, now(), :zcat, :zmember, :ztags)");
+
+					$stmt->execute(array(
+
+						'zname' 	=> $name,
+						'zdesc' 	=> $desc,
+						'zprice' 	=> $price,
+						'zcountry' 	=> $country,
+						'zstatus' 	=> $status,
+						'zcat'		=> $cat,
+						'zmember'	=> $member,
+						'ztags'		=> $tags
+
+					));
+
+					// Echo Success Message
+
+					$theMsg = "<div class='alert alert-success'>" . $stmt->rowCount() . ' Record Inserted</div>';
+
+					redirectHome($theMsg, 'back');
+
+				}
+
+			} else {
+
+				echo "<div class='container'>";
+
+				$theMsg = '<div class="alert alert-danger">Sorry You Cant Browse This Page Directly</div>';
+
+				redirectHome($theMsg);
+
+				echo "</div>";
+
+			}
+
+			echo "</div>";
  
 
 		} elseif ($do == 'Insert') {
+			
 
 
 		} elseif ($do == 'Edit') {
