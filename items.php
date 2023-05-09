23904066e2fed0a4b0d9deae9783383f22fa02c2
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2023-05-09 09:13:49 +0300

    Insert Edit

--- items.php
+++ items.php
@@ -17,10 +17,6 @@
 		$do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
 		if ($do == 'Manage') {
-<<<<<<< HEAD
-=======
-            echo'wellcome';
->>>>>>> cf27bb8aa635b6cc5e01ae0925e6e5faad0fc013
 
 
 			$stmt = $con->prepare("SELECT 
@@ -326,8 +322,221 @@
 			echo "</div>";
  
 
+	
+
+			echo "</div>";
+
 		} elseif ($do == 'Edit') {
 
+			// Check If Get Request item Is Numeric & Get Its Integer Value
+
+			$itemid = isset($_GET['itemid']) && is_numeric($_GET['itemid']) ? intval($_GET['itemid']) : 0;
+
+			// Select All Data Depend On This ID
+
+			$stmt = $con->prepare("SELECT * FROM items WHERE Item_ID = ?");
+
+			// Execute Query
+
+			$stmt->execute(array($itemid));
+
+			// Fetch The Data
+
+			$item = $stmt->fetch();
+
+			// The Row Count
+
+			$count = $stmt->rowCount();
+
+			// If There's Such ID Show The Form
+
+			if ($count > 0) { ?>
+
+<h1 class="text-center">Edit Item</h1>
+<div class="container">
+    <form class="form-horizontal" action="?do=Update" method="POST">
+        <input type="hidden" name="itemid" value="<?php echo $itemid ?>" />
+        <!-- Start Name Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Name</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="name" class="form-control" required="required" placeholder="Name of The Item"
+                    value="<?php echo $item['Name'] ?>" />
+            </div>
+        </div>
+        <!-- End Name Field -->
+        <!-- Start Description Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Description</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="description" class="form-control" required="required"
+                    placeholder="Description of The Item" value="<?php echo $item['Description'] ?>" />
+            </div>
+        </div>
+        <!-- End Description Field -->
+        <!-- Start Price Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Price</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="price" class="form-control" required="required" placeholder="Price of The Item"
+                    value="<?php echo $item['Price'] ?>" />
+            </div>
+        </div>
+        <!-- End Price Field -->
+        <!-- Start Country Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Country</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="country" class="form-control" required="required" placeholder="Country of Made"
+                    value="<?php echo $item['Country_Made'] ?>" />
+            </div>
+        </div>
+        <!-- End Country Field -->
+        <!-- Start Status Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Status</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="status">
+                    <option value="1" <?php if ($item['Status'] == 1) { echo 'selected'; } ?>>New</option>
+                    <option value="2" <?php if ($item['Status'] == 2) { echo 'selected'; } ?>>Like New</option>
+                    <option value="3" <?php if ($item['Status'] == 3) { echo 'selected'; } ?>>Used</option>
+                    <option value="4" <?php if ($item['Status'] == 4) { echo 'selected'; } ?>>Very Old</option>
+                </select>
+            </div>
+        </div>
+        <!-- End Status Field -->
+        <!-- Start Members Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Member</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="member">
+                    <?php
+										$allMembers = getAllFrom("*", "users", "", "", "UserID");
+										foreach ($allMembers as $user) {
+											echo "<option value='" . $user['UserID'] . "'"; 
+											if ($item['Member_ID'] == $user['UserID']) { echo 'selected'; } 
+											echo ">" . $user['Username'] . "</option>";
+										}
+									?>
+                </select>
+            </div>
+        </div>
+        <!-- End Members Field -->
+        <!-- Start Categories Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Category</label>
+            <div class="col-sm-10 col-md-6">
+                <select name="category">
+                    <?php
+										$allCats = getAllFrom("*", "categories", "where parent = 0", "", "ID");
+										foreach ($allCats as $cat) {
+											echo "<option value='" . $cat['ID'] . "'";
+											if ($item['Cat_ID'] == $cat['ID']) { echo ' selected'; }
+											echo ">" . $cat['Name'] . "</option>";
+											$childCats = getAllFrom("*", "categories", "where parent = {$cat['ID']}", "", "ID");
+											foreach ($childCats as $child) {
+												echo "<option value='" . $child['ID'] . "'";
+												if ($item['Cat_ID'] == $child['ID']) { echo ' selected'; }
+												echo ">--- " . $child['Name'] . "</option>";
+											}
+										}
+									?>
+                </select>
+            </div>
+        </div>
+        <!-- End Categories Field -->
+        <!-- Start Tags Field -->
+        <div class="form-group form-group-lg">
+            <label class="col-sm-2 control-label">Tags</label>
+            <div class="col-sm-10 col-md-6">
+                <input type="text" name="tags" class="form-control" placeholder="Separate Tags With Comma (,)"
+                    value="<?php echo $item['tags'] ?>" />
+            </div>
+        </div>
+        <!-- End Tags Field -->
+        <!-- Start Submit Field -->
+        <div class="form-group form-group-lg">
+            <div class="col-sm-offset-2 col-sm-10">
+                <input type="submit" value="Save Item" class="btn btn-primary btn-sm" />
+            </div>
+        </div>
+        <!-- End Submit Field -->
+    </form>
+
+    <?php
+
+					// Select All Users Except Admin 
+
+					$stmt = $con->prepare("SELECT 
+												comments.*, users.Username AS Member  
+											FROM 
+												comments
+											INNER JOIN 
+												users 
+											ON 
+												users.UserID = comments.user_id
+											WHERE item_id = ?");
+
+					// Execute The Statement
+
+					$stmt->execute(array($itemid));
+
+					// Assign To Variable 
+
+					$rows = $stmt->fetchAll();
+
+					if (! empty($rows)) {
+						
+					?>
+    <h1 class="text-center">Manage [ <?php echo $item['Name'] ?> ] Comments</h1>
+    <div class="table-responsive">
+        <table class="main-table text-center table table-bordered">
+            <tr>
+                <td>Comment</td>
+                <td>User Name</td>
+                <td>Added Date</td>
+                <td>Control</td>
+            </tr>
+            <?php
+								foreach($rows as $row) {
+									echo "<tr>";
+										echo "<td>" . $row['comment'] . "</td>";
+										echo "<td>" . $row['Member'] . "</td>";
+										echo "<td>" . $row['comment_date'] ."</td>";
+										echo "<td>
+											<a href='comments.php?do=Edit&comid=" . $row['c_id'] . "' class='btn btn-success'><i class='fa fa-edit'></i> Edit</a>
+											<a href='comments.php?do=Delete&comid=" . $row['c_id'] . "' class='btn btn-danger confirm'><i class='fa fa-close'></i> Delete </a>";
+											if ($row['status'] == 0) {
+												echo "<a href='comments.php?do=Approve&comid="
+														 . $row['c_id'] . "' 
+														class='btn btn-info activate'>
+														<i class='fa fa-check'></i> Approve</a>";
+											}
+										echo "</td>";
+									echo "</tr>";
+								}
+							?>
+            <tr>
+        </table>
+    </div>
+    <?php } ?>
+</div>
+
+<?php
+
+			// If There's No Such ID Show Error Message
+
+			} else {
+
+				echo "<div class='container'>";
+
+				$theMsg = '<div class="alert alert-danger">Theres No Such ID</div>';
+
+				redirectHome($theMsg);
+
+				echo "</div>";
+
+			}		
 
 		} elseif ($do == 'Update') {
 
